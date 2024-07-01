7ffcd4f609ef67af57e87533f8f39c0bc01b1653
LaloG19 <21362134@utcancun.edu.mx>
2024-07-01 11:25:43 -0500

    upgrade: cambios en los estilos del menú temporal

--- src/components/MenuTemporal.vue
+++ src/components/MenuTemporal.vue
@@ -1,63 +1,42 @@
 <script setup>
 import { ref } from 'vue';
 const modulos = ref([
+    { nombre: 'Login', ruta: '/login' },
     { nombre: 'Productos', ruta: '/productos' },
     { nombre: 'frmProductos - Layout Window', ruta: { name: 'formProducts'}},
-    { nombre: 'Login', ruta: '/login' },
-    { nombre: 'Horacio', ruta: '/helloword' },
     { nombre: 'Monedas', ruta: { name: 'coins' } },
     { nombre: 'Metodos y formas de pago', ruta:  { name: 'payment'}},
     { nombre: 'Tipo de comprobantes', ruta: { name: 'receipts'}},
     { nombre: 'Paises', ruta: { name: 'countries'}},
     { nombre: 'Impuestos SAT', ruta: { name: 'taxesSAT'}},
-
-    { nombre: 'Codigos Postales - SAT', ruta: { name: 'zipcode'}},
     
-    { nombre: 'Layout Window', ruta: { name: 'layWindow'}},
-    { nombre: 'Layout Standard', ruta: { name: 'layContent'}},
+    { nombre: 'Horacio', ruta: '/helloword' },
+    
+    { nombre: 'Codigos Postales - SAT', ruta: { name: 'zipcode'}},
+        
+/*     { nombre: 'Layout Window', ruta: { name: 'layWindow'}},
+    { nombre: 'Layout Standard', ruta: { name: 'layContent'}}, */
 ]);
 </script>
 
 <template>
-        <main class="Container bg-general">
+        <main class="bg-general flex flex-col items-center justify-center h-screen">
             <h1 class="bg-sidebar rounded-2xl px-8 py-4 text-white font-semibold text-[2.5rem]"> Menú temporal</h1>
 
-            <section class="Content flex flex-col bg-primary bg-opacity-90 min-w-[90vw] min-h-[70vh] mt-[3vh] rounded-2xl">
-                <ul>
-                    <li v-for="modulo in modulos" :key="modulo.nombre" class="min-w-[22vw] min-h-[2.5rem]">
-                        <router-link :to="modulo.ruta" class="flex items-center bg-warning pl-10 text-2xl font-semibold rounded-2xl text-left min-w-full min-h-[2.5rem]">{{ modulo.nombre }}</router-link>
-                    </li>
-                </ul>
-            </section>
+            <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 bg-navbar bg-opacity-90 min-w-[90vw] min-h-[70vh] mt-[3vh] rounded-2xl p-4">
+            <router-link 
+                :to="modulo.ruta" 
+                v-for="modulo in modulos" :key="modulo.nombre"
+                class="flex items-center bg-warning hover:bg-[#FFC107] px-6 transition-all duration-300
+                border-4 border-red-700 border-solid justify-center
+                text-2xl font-semibold rounded-2xl text-black no-underline
+                min-w-[25rem] max-w-96 min-h-[2.5rem]">
+                    {{ modulo.nombre }}
+            </router-link>
+</section>
+
         </main>
 </template>
 
 <style scoped>
-    .Container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-    }
-
-    .Content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-
-    ul {
-        list-style: none;
-    }
-
-    li {
-        margin: 1rem;
-    }
-
-    a {
-        text-decoration: none;
-        color: black;
-    }
 </style>
