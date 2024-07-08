d480b8e831586fc6947cb0673e571b02dbf7dd91
LaloG19 <21362134@utcancun.edu.mx>
2024-07-08 14:00:30 -0500

    up: Integración de los temas en el módulo de formas y métodos de pago

--- src/modules/SAT/payment-methods-forms/components/tablePaymentForms.vue
+++ src/modules/SAT/payment-methods-forms/components/tablePaymentForms.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref, watch } from 'vue';
+import { useTheme } from '@/commons/composables/useTheme';
+const { theme } = useTheme();
 
 const ListadoFormasPago = ref([]);
 
@@ -18,20 +20,19 @@ watch(() => props.ListadoFormasPago, (newValue) => {
 <template>
     <table class="w-full table-fixed leading-4 text-[1rem]">
         <thead>
-            <tr class="sticky top-0 h-primaryHeaderTableHeight">
-                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-tl-xl">Clave</th>
-                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-t-[1px]">Nombre</th>
-                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-t-[1px]">Bancarizado</th>
-                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-tr-xl">Status</th>
+            <tr class="sticky top-0 h-primaryHeaderTableHeight text-white">
+                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-tl-xl" :class="`bg-headerTable-${theme}`">Clave</th>
+                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-t-[1px]" :class="`bg-headerTable-${theme}`">Nombre</th>
+                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-t-[1px]" :class="`bg-headerTable-${theme}`">Bancarizado</th>
+                <th class="border-b-secondaryTableWidth border-b-primaryUnderline bg-primaryHeaderTable rounded-tr-xl" :class="`bg-headerTable-${theme}`">Status</th>
             </tr>
         </thead>
         <tbody>
             <tr class="text-primaryFontColor" v-for="(forma, index) in ListadoFormasPago" :key="index"
-                :class="{
-                    'bg-primaryBodyTable': (index % 2 === 1 && forma.Activo === 1),
-                    'bg-secondaryBodyTable': (index % 2 === 0 && forma.Activo === 1),
-                    'bg-disableBodyTable': (forma.Activo === 0), 'text-white': (forma.Activo === 0), 'font-medium': (forma.Activo === 0)
-                }">
+                :class="[
+                    forma.Activo === 1 ? `text-textTable-${theme} bg-white` : '',
+                    forma.Activo === 0 ? 'text-white bg-disabled' : ''
+                ]">
                 <td class="h-primaryBodyTableHeight border-l-primaryTableWidth border-b-secondaryTableWidth border-b-primaryUnderline px-2 text-center truncate">
                     {{ forma.ClaveFormaPago }}
                 </td>
