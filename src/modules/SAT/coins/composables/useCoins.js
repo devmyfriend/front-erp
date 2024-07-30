8c5bf4fe76f1947ae5786e99078e2f8f9ea9ec92
LaloG19 <21362134@utcancun.edu.mx>
2024-07-30 10:20:49 -0500

    refactor: Se movió la lógica del modulo de monedas hacía el composable useCoins

--- src/modules/SAT/coins/composables/useCoins.js
+++ src/modules/SAT/coins/composables/useCoins.js
@@ -3,20 +3,103 @@ import { coinsStore } from '@/store/coinsStore';
 const store = coinsStore();
 
 const ListadoMonedas = ref([]);
+const modalData = ref(null);
+const showModal = ref(false);
+const modoFormulario = ref(0);
+const bodyFrm = ref({
+    ClaveMoneda: null,
+    Descripcion: null,
+});
 
 const cargarMonedas = async () => {
     await store.cargarMonedas();
     ListadoMonedas.value = store.getMonedas;
 };
+
 const buscarMonedas = async (texto) => {
     await store.buscarMonedas(texto);
     ListadoMonedas.value = store.getMonedas;
 };
 
+const cargarDatos = async () => {
+    await cargarMonedas();
+    modoFormulario.value = 0;
+    showModal.value = false;
+    bodyFrm.value = {
+        ClaveMoneda: null,
+        Descripcion: null,
+    };
+};
+
+const esperarTabla = (data) => {
+    const [moneda, accion] = data;
+    if (accion === 1) {
+        subirDatos(moneda);
+    } else if (accion === 2) {
+        modalData.value = moneda.ClaveMoneda;
+        console.log('Se va a borrar la moneda: ', moneda.ClaveMoneda);
+    }
+};
+
+const esperarBusqueda = async (texto) => {
+    if (texto) {
+        await buscarMonedas(texto);
+    } else {
+        await cargarDatos();
+    }
+};
+
+const subirDatos = (datos) => {
+    bodyFrm.value = { ...datos };
+    showModal.value = true;
+    modoFormulario.value = 1;
+};
+
+const esperarModal = (datos) => {
+    if (modoFormulario.value === 0) {
+        store.crearMoneda(bodyFrm.value).then(() => {
+            cargarDatos();
+        });
+    } else if (modoFormulario.value === 1) {
+        console.log('El modal pidió actualizar con los datos: ' + JSON.stringify(datos));
+        store.actualizarMoneda(datos).then(() => {
+            cargarDatos();
+        });
+    }
+};
+
+const borrarRegistro = () => {
+    store.eliminarMoneda(modalData.value).then(() => {
+        cargarDatos();
+        modalData.value = null;
+    });
+};
+
+const esperarCancelar = () => {
+    modalData.value = null;
+    showModal.value = false;
+    modoFormulario.value = 0;
+    bodyFrm.value = {
+        ClaveMoneda: null,
+        Descripcion: null,
+    };
+};
+
 export function useCoins() {
     return {
         ListadoMonedas,
+        modalData,
+        showModal,
+        modoFormulario,
+        bodyFrm,
         cargarMonedas,
         buscarMonedas,
+        cargarDatos,
+        esperarTabla,
+        esperarBusqueda,
+        subirDatos,
+        esperarModal,
+        borrarRegistro,
+        esperarCancelar,
     };
 }
