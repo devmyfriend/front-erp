aecdc6796e005fea8b21889b39105fa00845af7a
LaloG19 <21362134@utcancun.edu.mx>
2024-08-19 15:04:27 -0500

    fix: Se comentó la importación y el componente de frmGeneralData

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-  import frmGeneralData from '@/modules/businessentity/components/frmGeneralData.vue'
+  /* import frmGeneralData from '@/modules/businessentity/components/frmGeneralData.vue' */
 </script>
 
 <template>
   <div >
     <p>prueba</p>
-    <frmGeneralData />
+    <!-- <frmGeneralData /> -->
   </div>
 </template>
 
