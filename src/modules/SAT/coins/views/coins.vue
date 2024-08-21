cf66a2dbb4155804a245f1bf675f66044e79318f
LaloG19 <21362134@utcancun.edu.mx>
2024-08-21 15:37:09 -0500

    refactor: Se modificó el modal para dar de alta o actualizar monedas, ahora se maneja el modalGeneral.

--- src/modules/SAT/coins/views/coins.vue
+++ src/modules/SAT/coins/views/coins.vue
@@ -4,7 +4,7 @@ import tableCoins from '@/modules/SAT/coins/components/tableCoins.vue';
 import coinFinder from '@/modules/SAT/coins/components/coinFinder.vue';
 import deleteModal from '@/commons/ui/modals/deleteModal.vue';
 import btnFormulario from '@/commons/ui/btn-formulario/btn-formulario.vue';
-import coinsModal from '@/commons/ui/modals/coinsModal.vue';
+import generalModal from '@/commons/ui/modals/generalModal.vue';
 import { useLayout } from '@/commons/composables/useLayout.js';
 import { useCoins } from '@/modules/SAT/coins/composables/useCoins.js';
 import { useTheme } from '@/commons/composables/theme';
@@ -41,8 +41,23 @@ onBeforeMount(() => {
         <tableCoins :editable="true" @eAccion="esperarTabla" />
     </div>
     <deleteModal :id="modalData" v-if="modalData !== null" @eEliminar="borrarRegistro" @eCancelar="esperarCancelar" />
-    <coinsModal :datos="bodyFrm" :mostrar="showModal" :modo="modoFormulario" @eConfirm="esperarModal"
-        @eCancel="esperarCancelar" />
+    <generalModal v-if="showModal" @eConfirm="esperarModal" @eCancel="esperarCancelar" :datos="bodyFrm">
+        <template #header>
+            {{ modoFormulario === 1 ? 'Crear' : 'Editar' }} moneda
+        </template>
+        <template #content>
+            <div class="flex flex-col justify-center items-start">
+                <label for="Clave" class="text-sm font-bold text-gray-700 relative px-1">Clave Moneda:</label>
+                <input :disabled="modoFormulario === 1" name="Clave" v-model="bodyFrm.ClaveMoneda" type="text"
+                    placeholder="MXN"
+                    class="mt-1 block w-full text-sm border-2 border-gray-400 transition-all duration-300 focus:border-gray-800 box-border p-2 outline-none"
+                    :class="{ [`bg-${theme}-disabled`]: modoFormulario === 1, 'bg-white': modoFormulario !== 1 }">
+                <label class="text-sm font-bold text-gray-700 relative px-1 mt-4">Descripción</label>
+                <input v-model="bodyFrm.Descripcion" type="text" placeholder="Peso Mexicano"
+                    class="mt-1 mb-1 block w-full text-sm border-2 border-gray-400 transition-all duration-300 focus:border-gray-800 box-border p-2 outline-none">
+            </div>
+        </template>
+    </generalModal>
 </template>
 
 <style scoped></style>
