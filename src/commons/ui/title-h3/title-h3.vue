1d47f3bef6e83c7906a048f8d68351bf3a381495
LaloG19 <21362134@utcancun.edu.mx>
2024-08-23 15:30:10 -0500

    style: Se incrementó el tamaño mínimo del texto en el componente title-h3

--- src/commons/ui/title-h3/title-h3.vue
+++ src/commons/ui/title-h3/title-h3.vue
@@ -4,7 +4,7 @@ const { theme } = useTheme();
 </script>
 
 <template>
-    <h3 class="text-[0.75rem] lg:text-[1.25rem] font-bold w-full mb-2" :class="`text-${theme}-subtext`">
+    <h3 class="text-[1rem] lg:text-[1.25rem] font-bold w-full mb-2" :class="`text-${theme}-subtext`">
         <slot></slot>
     </h3>
 </template>
