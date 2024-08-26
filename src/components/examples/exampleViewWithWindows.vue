d1da1a5a526e1bb15ccd80acfb33214d9380187a
LaloG19 <21362134@utcancun.edu.mx>
2024-08-26 09:21:48 -0500

    fix: Se corrigieron los examples por un problema de doble seteo de tamaños

--- src/components/examples/exampleViewWithWindows.vue
+++ src/components/examples/exampleViewWithWindows.vue
@@ -30,7 +30,7 @@ onBeforeMount(() => {
                 <div class="flex flex-row mb-6 h-inputHeight gap-4">
                     <coinFinder @eBusqueda="esperarBusqueda" />
                 </div>
-                <btnFormulario @eNuevo="showModal = true; modoFormulario = 0"/>
+                <btnFormulario @eNuevo="showModal = true; modoFormulario = 0" />
             </div>
             -->
         <!-- Versión con buscador y botón de agregar -->
@@ -38,8 +38,7 @@ onBeforeMount(() => {
         <!-- Versión sin buscador y botón de agregar -->
             <!--
                 <div class="flex mb-6 h-inputHeight justify-end">
-                    <btnFormulario @eNuevo="showModal = true; modoFormulario = 0"
-                        class="text-base px-4 py-2 text-white rounded-md uppercase font-semibold flex items-center justify-center transition-all duration-300 max-w-24 min-w-24 md:max-w-32 md:min-w-32 xl:max-w-36 xl:min-w-36" />
+                    <btnFormulario @eNuevo="showModal = true; modoFormulario = 0" />
                 </div>
             -->
         <!-- Versión sin buscador y botón de agregar -->
