5faea19b1e5266fdb877f45bb971102f4f9789e1
LaloG19 <21362134@utcancun.edu.mx>
2024-08-29 16:00:53 -0500

    fix: Corrección de formato y tamaño de la imagen del módulo de frmDataProducts

--- src/modules/products/components/frmImage.vue
+++ src/modules/products/components/frmImage.vue
@@ -14,8 +14,8 @@ const esperarImagen = (archivo) => {
 <template>
     <div class="flex flex-col justify-center items-center rounded-xl border-2 p-4 w-5/6 md:w-3/4 lg:w-2/4 xl:w-1/3 gap-4 2xl:w-2/6"
         :class="`bg-${theme}-container border-${theme}-primary`">
-        <div class="overflow-hidden size-full">
-            <img :src="imagenProducto" alt="Image" class="size-full object-contain">
+        <div class="overflow-hidden size-full content-center">
+            <img :src="imagenProducto" alt="Image" class="w-full object-cover">
         </div>
         <btnFile @eGuardar="esperarImagen" />
     </div>
